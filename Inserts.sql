3143c6c2e04c3f8884c4f18e7a9877e7e9a1fe88
Huseyin <adesproj@gmail.com>
2015-03-16 21:53:30 +0000

    sql

---
+++ Inserts.sql
@@ -0,0 +1,24 @@
+INSERT INTO `global_location` (`city`, `country`) VALUES ('London', 'UK');
+INSERT INTO `global_location` (`city`, `country`) VALUES ('Manchester', 'UK');
+INSERT INTO `global_location` (`city`, `country`) VALUES ('Barcelona', 'Spain');
+INSERT INTO `global_location` (`city`, `country`) VALUES ('Bodrum', 'Turkey');
+INSERT INTO `global_location` (`city`, `country`) VALUES ('Sydney', 'Australia');
+INSERT INTO `global_location` (`city`, `country`) VALUES ('New York', 'USA');
+INSERT INTO `global_location` (`city`, `country`) VALUES ('Tunis', 'Tunisia');
+INSERT INTO `global_location` (`city`, `country`) VALUES ('Paris', 'France');
+INSERT INTO `global_location` (`city`, `country`) VALUES ('Runteburgh', 'Portugal');
+INSERT INTO `global_location` (`city`, `country`) VALUES ('Harrisonfurt', 'Burkina Faso');
+INSERT INTO `global_location` (`city`, `country`) VALUES ('Istanbul', 'Turkey');
+
+INSERT INTO `global_tickettype` (`tktType`) VALUES ('Child');
+INSERT INTO `global_tickettype` (`tktType`) VALUES ('Teen');
+INSERT INTO `global_tickettype` (`tktType`) VALUES ('Student');
+INSERT INTO `global_tickettype` (`tktType`) VALUES ('Adult');
+INSERT INTO `global_tickettype` (`tktType`) VALUES ('Senior');
+
+INSERT INTO `global_attractiontype` (`attType`) VALUES ('Sightseeing');
+INSERT INTO `global_attractiontype` (`attType`) VALUES ('Theatre');
+INSERT INTO `global_attractiontype` (`attType`) VALUES ('Theme Park');
+
+
+
