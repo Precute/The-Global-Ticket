9ca30142c87f41297447a9184bedc70d6f3e9e23
Huseyin <harpalikli@hotmail.com>
2015-03-24 14:05:34 +0000

    inserts

--- Inserts.sql
+++ Inserts.sql
@@ -9,6 +9,7 @@ INSERT INTO `global_location` (`city`, `country`) VALUES ('Paris', 'France');
 INSERT INTO `global_location` (`city`, `country`) VALUES ('Runteburgh', 'Portugal');
 INSERT INTO `global_location` (`city`, `country`) VALUES ('Harrisonfurt', 'Burkina Faso');
 INSERT INTO `global_location` (`city`, `country`) VALUES ('Istanbul', 'Turkey');
+INSERT INTO `arpalikh`.`global_location` (`city`, `country`) VALUES ('Stoke-on-Trent', 'UK');
 
 INSERT INTO `global_tickettype` (`tktType`) VALUES ('Child');
 INSERT INTO `global_tickettype` (`tktType`) VALUES ('Teen');
@@ -20,7 +21,12 @@ INSERT INTO `global_attractiontype` (`attType`) VALUES ('Sightseeing');
 INSERT INTO `global_attractiontype` (`attType`) VALUES ('Theatre');
 INSERT INTO `global_attractiontype` (`attType`) VALUES ('Theme Park');
 
-INSERT INTO `arpalikh`.`global_employeeaccount` (`empFirstname`, `empSurname`, `empDOB`, `empStartDate`, `empRole`, `empSalary`, `empEmail`, `empTel`) 
-VALUES ('Precute', 'Karim', '1994-10-03', '2014-12-30', 'Manager', '16000', 'p.karim@yahoo.com', '0161600000');
-INSERT INTO `arpalikh`.`global_employeeaccount` (`empFirstname`, `empOthername`, `empSurname`, `empDOB`, `empStartDate`, `empSalary`, `empEmail`,`managerID` ) 
-VALUES ('Grace', 'Helen', 'John', '1995-11-04-', '2001-01-10', '14300', 'g.john@globalticket.com', 1);
+INSERT INTO `arpalikh`.`global_employeeaccount` (`empFirstname`, `empSurname`, `empDOB`, `empStartDate`, `empRole`, `empSalary`, `empEmail`, `empTel`) VALUES ('Precute', 'Karim', '1994-10-03', '2014-12-30', 'Manager', '16000', 'p.karim@yahoo.com', '0161600000');
+INSERT INTO `arpalikh`.`global_employeeaccount` (`empFirstname`, `empOthername`, `empSurname`, `empDOB`, `empStartDate`, `empSalary`, `empEmail`,`managerID` ) VALUES ('Grace', 'Helen', 'John', '1995-11-04-', '2001-01-10', '14300', 'g.john@globalticket.com', 1);
+
+
+INSERT INTO `arpalikh`.`global_attractioncatalogue` (`attName`, `attDescript`, `attAvailabilityCount`, `attTypeID`, `locationID`) VALUES ('Alton Towers', 'Exciting theme park in the North West of England!', '100000', '3', '12');
+
+
+INSERT INTO `arpalikh`.`global_attractionpricelist` (`attractionID`, `tktTypeID`, `ticketPrice`) VALUES ('1', '1', '25');
+INSERT INTO `arpalikh`.`global_attractionpricelist` (`attractionID`, `tktTypeID`, `ticketPrice`) VALUES ('1', '4', '45');
